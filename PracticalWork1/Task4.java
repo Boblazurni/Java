ed94651ceba6711734267065c84c43428d8866c8
unknown <shershnevar2006@gmail.com>
2025-09-23 08:37:13 +0300

    PW1 Task4 upd 2

--- PracticalWork1/Task4.java
+++ PracticalWork1/Task4.java
@@ -3,23 +3,20 @@ import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] args) {
-        int min = 1;
-        int max = 1;
+        int min = 1, max = 1, sum = 0;
         Scanner scanner = new Scanner(System.in);
         int length = scanner.nextInt();
         int[] arr = new int[length]; 
         int i = 0;
 
         while (i < length) {
-            if(i == 1){
-                max = arr[0];
-                min = arr[0];
-            }
             arr[i] = scanner.nextInt();
-            if(arr[i] > max) max = arr[i];
-            if(arr[i] < min) max = arr[i];
+            sum += arr[i];
+            if (i == 0) { max = arr[0]; min = arr[0]; }
+            max = (arr[i] > max) ? arr[i] : max; //short if
+            min = (arr[i] < min) ? arr[i] : min;
             i++;
         }
-        System.out.println(max + min);
+        System.out.println("Sum: " + sum + ", Max: " + max + ", Min: " + min);
     }
 }
